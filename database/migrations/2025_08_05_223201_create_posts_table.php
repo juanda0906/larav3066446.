4abff0c5e1fae2e91944bd29f839e155eea7b9f0
Manuel Alejandro <mplaza005@gmail.com>
2025-09-02 16:14:11 -0500

    migraciones con fk

--- database/migrations/2025_08_05_223201_create_posts_table.php
+++ database/migrations/2025_08_05_223201_create_posts_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('body');
+            //Atributos foraneos
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('categoria_id')->nullable();
+           //referenciando la tabla users
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->onDelete('set null');
+            //referenciando la tabla categorias
+            $table->foreign('categoria_id')
+            ->references('id')
+            ->on('categories')->onDelete('set null');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,15 @@ use App\Http\Controllers\OperacionesController;
 
 
 
+
 Route::get('saludo', [SaludoController::class, 'saludar']);
 
 Route::get('suma/{num1}/{num2}', [OperacionesController::class, 'sumar']);
 
 Route::get('frmcuadratica', [OperacionesController::class, 'frmcuadratica']);
 Route::post('cuadratica', [OperacionesController::class, 'cuadratica'])->name('cudratica.calcular');
+
+
+
+Route::get('create', [CategoryController::class, 'create']);
+Route::post('store', [CategoryController::class, 'store'])->name('category.store');
