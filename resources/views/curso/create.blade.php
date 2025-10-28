193ad5fd48f4c51982d9c92880bf82bd154faa50
Manuel Alejandro <mplaza005@gmail.com>
2025-10-28 16:25:36 -0500

    lienzos

--- resources/views/curso/create.blade.php
+++ resources/views/curso/create.blade.php
@@ -1,13 +1,7 @@
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('content')
 
-<body>
     <h1>Crear Curso</h1>
 
     <form action="{{ route('curso.store') }}" method="POST" enctype="multipart/form-data">
@@ -35,6 +29,6 @@
         <button type="submit">Registrar Curso</button>
     </form>
 
-</body>
 
-</html>
+@endsection
+
--- resources/views/curso/index.blade.php
+++ resources/views/curso/index.blade.php
@@ -1,11 +1,6 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
 
     <h1>Listar cursos</h1>
 
@@ -32,5 +27,5 @@
     </tr>
 @endforeach
 
-</body>
-</html>
+
+@endsection
